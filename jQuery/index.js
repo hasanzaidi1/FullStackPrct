a7481d29676b4b1a28988a96c014545b7c9712d3
hasanzaidi1 <hasanammarza@usf.edu>
2024-07-01 10:26:14 -0400

    event listener

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,4 +1,16 @@
 
 
-$("h1").css("color", "red");
+// $("h1").addClass("big-title");
+
+
+
+$("button").click(function(){
+    $("h1").css("color", "purpule");
+})
+
+$("h1").click(function() {
+    $("h1").css("color", "green");
+})
+
+
 
