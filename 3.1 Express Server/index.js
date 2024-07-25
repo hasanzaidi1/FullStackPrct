48771851e06ced76f297b1f4a557d7b288c20e5e
hasanzaidi1 <hasanammarza@usf.edu>
2024-07-25 11:19:28 -0400

    Express Server

--- 3.1 Express Server/index.js
+++ 3.1 Express Server/index.js
@@ -1,9 +1,21 @@
-import express from "express";
+import express, { raw } from "express";
 
 
 const app = express()
 const port = 3001;
 
+
+app.get("/", (req,res) => {
+    res.send("<h1>Willkommen</h1><a href='http://localhost:3001/about'>about</a>");
+})
+
+app.get("/about", (req,res) => {
+    res.send("<h1>About Page</h1><a href='http://localhost:3001/contact'>contact</a>");
+})
+
+app.get("/contact", (req,res) => {
+    res.send("<h1>Contact Page</h1><a href='http://localhost:3001/'>home</a>");
+})
 
 app.listen(port, () => {
     console.log(`server running on port ${port}`)
